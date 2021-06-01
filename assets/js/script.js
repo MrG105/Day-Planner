b1ae282197bff8cef6abf723f611a5bb01137887
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-05-31 23:51:09 -0500

    updated javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,40 @@
 
 var currentDay = moment().format('dddd, MMM Do YYYY');
 $('#currentDay').html(currentDay);
+
+$('#workhours').children().each(function(index, element) {
+    var eachHour = parseInt($(this).data("hour"));
+    timeColor(eachHour, $(this))
+})
+
+function timeColor(momentHour, timeBlock) {
+    var currentHour = moment().hour();
+    if (momentHour === currentHour) {
+        timeBlock.addClass('present');
+        timeBlock.removeClass('past future');
+    } else if (momentHour < currentHour) {
+        timeBlock.addClass('past');
+        timeBlock.removeClass('present future')
+    } else {
+        timeBlock.addClass('future');
+        timeBlock.removeClass('present past')
+    }
+}
+
+$('.saveBtn').click(function () { 
+    var text = $(this).siblings('.description').val();
+    var hour = $(this).parent().attr('id');
+    localStorage.setItem(hour, text);
+})
+
+function init() {
+    $('#workhours').children().each(function(index, element) {
+        var localHour = $(this).attr('id');
+        var storedAppt = localStorage.getItem(localHour);
+        if (storedAppt !== null) {
+            $(this).children('.description').val(storedAppt)
+        } 
+    })
+}
+
+init();
